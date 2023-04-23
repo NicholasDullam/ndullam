af5291807545a8af608cb748368fb9cdff63e9e1
Nicholas <npdullam@gmail.com>
2023-04-22 20:44:13 -0400

    last changeS

--- client/src/pages/TrafficRL.jsx
+++ client/src/pages/TrafficRL.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import trafficrl1 from '../images/trafficrl1.png'
 import { Social, Tag } from '../components'
 
+import modelingreport from '../images/modelingreport.pdf'
+
 const TrafficRL = (props) => {
     return (
         <div>
@@ -19,13 +21,19 @@ const TrafficRL = (props) => {
                 </div>
                 <h6 className="mt-5"> A project aimed at optimizing traffic-light patterns for improved realistic throughput of intersections. </h6>
                 <h3 className="text-xl font-bold mt-5 mb-2"> Description </h3>
-                <p> Communicode was a start-up project that used machine learning to connect developers and designers with non-profit organizations in need of a digital update. Designers and developers would be matched with non-profit organizations based on their interests and skills, or they could browse projects posted by non-profits. </p>
+                <p> As a capstone project for CS 490, my team and I worked alongside a professor from Purdue's department of Civil Engineering to optimize traffic patterns with deep-reinforcement learning principles. </p>
                 <h3 className="text-xl font-bold mt-5 mb-2"> Role |<span className="text-base opacity-50"> Member </span> </h3>
-                At Communicode, I served as the Vice President of Engineering; overseeing integrations, server architectures, and ensuring the continuity of the backend development process as we expanded to an LLC. 
-                <h3 className="text-xl font-bold mt-5 mb-2"> Creating <i>Shapiro</i> </h3>
-                <p> One of my major feats at Communicode was the development of a backend matching algorithm; namely, <i>Shapiro</i>. <i>Shapiro</i> was an algorithm, written in python, based upon the principles of k-nearest neighbors clustering with adaptions driven towards optimizing our user experience. </p>
+                <p> As a member of a team of six other developers, I worked to aid the group in data preparation, modeling, evaluations, and general understanding, while documenting reports of our results and processes for further repeatability. </p>
+                <h3 className="text-xl font-bold mt-5 mb-2"> Modeling with Deep RL </h3>
+                <p> In our case, given our use of reinforcement learning, it's important to note how we managed the state and environment. Based on 15 minutes of data from Lankershim boulevard, we created baseline aggregates, like turning volume, to use an a simulation tool known as SUMO -- this would end up being our way of creating a new data pool for our models.  Modeling with a focus on deep-reinforcement learning in our case can be broken down into three groups: use of deep-q networks (DQN), proximal policy optimization (PPO), and soft-actor critic (SAC). Each of these models was tested with two value functions to optimize throughout, one with judgement on aggregate vehicle delay, and the other on total vehicle stops. Results for our initial findings can also be found below.  </p>
+                <div style={{ color: 'white', marginTop: '20px', display: 'flex' }}>
+                    <Social name="Dataset" link="https://data.transportation.gov/Automobiles/Next-Generation-Simulation-NGSIM-Vehicle-Trajector/8ect-6jqj"/>
+                </div>
+                <div style={{ color: 'white', marginTop: '20px', display: 'flex' }}>
+                    <Social name="Modeling" link={modelingreport}/>
+                </div>
                 <h3 className="text-xl font-bold mt-5 mb-2"> Key Takeaways </h3>
-                <p> Working at Communicode was one of my first real-world application of what I've learned throughout my life programming. I had never been exposed to web development, nor an outside work environment before this point. What I learned here pushed me into where I am today, studying Computer Science, Data Science, and Applied Statistics at Purdue. </p>
+                <p> As a project heavy in development, understanding, and repeatability, it was important to cycle changes -- for instance, once we've developed a solution, but develop a new understanding of the data, it's crucial to cycle back and reflect the new understanding in the development process. As for repeatability, with things constantly changing, it's necessary to document the entire process to ensure any results can be matched by a reviewing third-party. </p>
             </div>
         </div>
     )
