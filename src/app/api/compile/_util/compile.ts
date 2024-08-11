d4a4eb812fa3b0f44841d05bcae22f0cd0939e1d
Nicholas <npdullam@gmail.com>
2024-08-10 23:04:51 -0400

    moved codegen

--- src/app/api/compile/_util/compile.ts
+++ src/app/api/compile/_util/compile.ts
@@ -1,13 +1,18 @@
 import { exec } from "child_process";
 import path from "path";
 
+export type CompileResponse = {
+  response: string;
+  time: number;
+};
+
 export const compile = async (dir: string) =>
-  new Promise<{ response: string; time: number }>((resolve, reject) => {
+  new Promise<CompileResponse>((resolve, reject) => {
     const baseline = Date.now();
     return exec(
       `./codegen ${dir}`,
       {
-        cwd: path.join(__dirname, "../../../../..", "compiler"),
+        cwd: path.join(__dirname, "../../../../../.."),
       },
       (error, stdout, stderr) => {
         if (error) return reject(stderr.toString());
