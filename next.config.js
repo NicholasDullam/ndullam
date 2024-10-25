453c7ec16ece49d035222be38adcd63dd1eb3b53
Nicholas <npdullam@gmail.com>
2024-10-25 19:18:58 -0400

    add codegen to outputfile tracing

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    outputFileTracingIncludes: {
+      "api/compile": ["./codegen"],
+    },
+  },
 };
 
 module.exports = nextConfig;
