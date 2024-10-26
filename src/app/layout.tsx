f2d43cb190b8fd6e225a647117a584e469c01de9
Nicholas <npdullam@gmail.com>
2024-10-25 20:12:16 -0400

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { ReactNode } from "react";
 
 import "../../index.css";
@@ -9,7 +10,10 @@ type RootLayoutProps = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body className="dark">{children}</body>
+      <body className="dark">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
